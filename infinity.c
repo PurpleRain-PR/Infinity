618b360177aa2f82edece3f9a125d0cc65c16bc4
pixelroll <8fbe18fbf5c9269b18b88093ea4bdc38>
2025-07-16 17:18:38 +0800

    初始化（的变量设置初值部分

--- infinity.c
+++ infinity.c
@@ -6,5 +6,25 @@ void test(void)
 {
     printf("Infinity\n");
 }
+
+bool initInfNum(const PInfNum num, const _InfNumType type){
+    if(!type)return false;
+    num->info.type=type;
+
+    num->info.sign=INF_POSITIVE;
+    num->info.intNodeAllocateCount=0;
+
+    num->intPart->length=0;
+    if(!(type&INF_INT)){
+        num->intPart=nullptr;
+    }
+    if(!(type&INF_FRACT)){
+        num->fractPart=nullptr;
+    }
+    return true;
+}
+
+
+
 //test
 
--- infinity.h
+++ infinity.h
@@ -6,9 +6,23 @@
 
 void test(void);
 
+//类型
+typedef  unsigned char _InfNumType;
+#define INF_INT (1)
+#define INF_FRACT (2)
+#define INF_INT_FRACT (3)
+
+//正负
+typedef  unsigned char _InfNumSign;
+#define INF_POSITIVE (0)
+#define INF_NEGATIVE (1)
+
 typedef struct _InfNumInfo
 {
     unsigned int sign : 1;
+    unsigned int type : 2;//高位代表整数，低位代表小数，即1:纯小数,2:纯整数,3:整数与小数
+    unsigned int intNodeAllocateCount:4;//上一次节点合并重组后新开辟数据节点的次数，用于自动合并重组，每15次时达到强制重组标准，若没有整数部分，该值为0
+
 } InfNumInfo, *PInfNumInfo;
 
 typedef struct _InfinityIntegerDataNode
@@ -33,6 +47,10 @@ typedef struct _InfinityFract
 
 typedef struct _InfinityNumber
 {
+    InfNumInfo info;
+    PInfInt intPart;
+    PInfFract fractPart;
+
 } InfNum, *PInfNum;
 
 #endif //INFINITY_LIBRARY_H

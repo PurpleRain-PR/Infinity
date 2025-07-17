144010b098e561c9abc681cc54abf594ef4ac40b
pixelroll <8fbe18fbf5c9269b18b88093ea4bdc38>
2025-07-17 19:46:56 +0800

    整数索引区扩充函数完成

--- infinity.c
+++ infinity.c
@@ -9,33 +9,79 @@ void test(void)
     printf("Infinity\n");
 }
 
-bool initInfNum(const PInfNum num, const _InfNumType type, _InfNumSign sign){
-    if(!type)return false;//类型错误
-    num->info.type=type;
-    num->info.sign=sign;
-    num->info.intNodeAllocateCount=0;
-    if(type&INF_INT){
-        num->intPart=malloc(sizeof(InfInt));
-        if (!num->intPart)return false;//无法分配内存
-        num->intPart->firstDataNode=nullptr;
-        num->intPart->nodeCount=0;
+bool initInfNum(PInfNum num, _InfNumType type, _InfNumSign sign)
+{
+    num->info.type = type;
+    num->info.sign = sign;
+    num->info.intNodeAllocateCount = 0;
+    if (type & INF_INT)
+    {
+        num->intPart = malloc(sizeof(InfInt));
+        if (!num->intPart)
+        {
+            num->info.error = true;
+            return false;
+        } //无法分配内存
+        num->intPart->firstDataNode = nullptr;
+        num->intPart->lastDataNode = nullptr;
+        num->intPart->nodeCount = 0;
     }
-    else{
-        num->intPart=nullptr;
+    else
+    {
+        num->intPart = nullptr; //未使用则置空
     }
-    if(type&INF_FRACT){
-        num->fractPart=malloc(sizeof(InfFract));
-        if (!num->fractPart)return false;//无法分配内存
-        num->fractPart->value=nullptr;
-        num->fractPart->byteLength=0;
+    if (type & INF_FRACT)
+    {
+        num->fractPart = malloc(sizeof(InfFract));
+        if (!num->fractPart)
+        {
+            num->info.error = true;
+            return false;
+        } //无法分配内存
+        num->fractPart->value = nullptr;
+        num->fractPart->byteLength = 0;
     }
-    else{
-        num->fractPart=nullptr;
+    else
+    {
+        num->fractPart = nullptr; //未使用则置空
+    }
+    return true;
+}
+
+bool extendIntPart(PInfNum num, unsigned short byteLength)
+{
+    PInfIntDataNode newNode = malloc(sizeof(InfIntDataNode));
+    if (!newNode)
+    {
+        num->info.error = true;
+        return false;
+    } //无法分配内存
+    newNode->nextNode = nullptr;
+    newNode->data = malloc(byteLength);
+    if (!newNode->data)
+    {
+        num->info.error = true;
+        return false;
+    } //无法分配内存
+    num->intPart->nodeCount++;
+    newNode->byteLength = byteLength;
+    if (num->intPart->nodeCount)
+    {
+        newNode->prevNode = num->intPart->lastDataNode;
+        newNode->prevNode->nextNode = newNode;
+        num->intPart->lastDataNode = newNode;
+    }
+    else
+    {
+        num->intPart->firstDataNode = newNode;
+        num->intPart->lastDataNode = newNode;
+    }
+    if (num->info.intNodeAllocateCount<INF_MAX_NODE_ALLOC_COUNT)
+    {
+        num->info.intNodeAllocateCount++;
     }
     return true;
 }
 
 
-
 //test
-
--- infinity.h
+++ infinity.h
@@ -1,5 +1,5 @@
-#pragma once
-#include <stdint.h>
+// #pragma once
+// #include <stdint.h>
 
 #ifndef INFINITY_LIBRARY_H
 #define INFINITY_LIBRARY_H
@@ -17,32 +17,36 @@ typedef const unsigned char _InfNumSign;
 #define INF_POSITIVE (0)
 #define INF_NEGATIVE (1)
 
-typedef struct _InfNumInfo//数字的元数据
-{
-    unsigned sign : 1;//符号，如宏所示
-    unsigned type : 2;//数字类型，如宏所示，高位代表整数，低位代表小数，即1:纯小数,2:纯整数,3:整数与小数
-    unsigned intNodeAllocateCount:4;//上一次节点合并重组后新开辟数据节点的次数，用于自动合并重组，每15次时达到强制重组标准，若没有整数部分，该值为0
+//内存管理
+#define INF_MAX_NODE_ALLOC_COUNT (15)
 
+typedef struct _InfNumInfo //数字的元数据
+{
+    unsigned sign : 1; //符号，如宏所示
+    unsigned type : 2; //数字类型，如宏所示，高位代表整数，低位代表小数，即1:纯小数,2:纯整数,3:整数与小数
+    unsigned intNodeAllocateCount : 4; //上一次节点合并重组后新开辟数据节点的次数，用于自动合并重组，每15次时达到强制重组标准，若没有整数部分，该值为0
+    unsigned error : 1; //记录运行时是否发生错误,为真则该数据不可用
 } InfNumInfo, *PInfNumInfo;
 
-typedef struct _InfinityIntegerDataNode//整数数据链表的节点
+typedef struct _InfinityIntegerDataNode //整数数据链表的节点
 {
     unsigned short byteLength;
-    unsigned char* data;//指向一个数组
+    unsigned char* data; //指向一个数组
     struct _InfinityIntegerDataNode* nextNode;
-    struct _InfinityIntegerDataNode* lastNode;
+    struct _InfinityIntegerDataNode* prevNode;
 } InfIntDataNode, *PInfIntDataNode;
 
 typedef struct _InfinityInteger
 {
     unsigned char nodeCount;
     PInfIntDataNode firstDataNode;
+    PInfIntDataNode lastDataNode;
 } InfInt, *PInfInt;
 
 typedef struct _InfinityFract
 {
     unsigned char byteLength;
-    unsigned char * value;
+    unsigned char* value;
 } InfFract, *PInfFract;
 
 typedef struct _InfinityNumber
@@ -50,7 +54,6 @@ typedef struct _InfinityNumber
     InfNumInfo info;
     PInfInt intPart;
     PInfFract fractPart;
-
 } InfNum, *PInfNum;
 
-#endif //INFINITY_LIBRARY_H
+#endif INFINITY_LIBRARY_H
